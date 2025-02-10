4f2e3def0d20f7958c0db2524d275c80d018cdec
PARK, Yeonjong <yeonjong.park@kumoh.ac.kr>
2025-02-10 21:34:13 +0900

    프로젝트 환경 준비

---
+++ src/test/java/com/patulus/learn_jwt/LearnJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patulus.learn_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
